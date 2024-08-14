04780957d773bb1cc5ed5c208b7ae6401bef582f
bwramsey <ramsey.313@math.osu.edu>
2024-08-14 09:48:56 -0400

    script folders

---
+++ scripts/xmlatex.bat
@@ -0,0 +1,7 @@
+@echo off
+set str=%*
+set "str=%str:\=/%"
+set "DOCKER_IMAGE=ghcr.io/ximeraproject/xake2023:v2.1.1"
+echo Starting docker %DOCKER_IMAGE% with args  %str%  in directory %cd%.
+echo docker run --rm --net="host" --mount type=bind,source=%cd%,target=/code %DOCKER_IMAGE% xmlatex %str%
+docker run --rm --net="host" --mount type=bind,source=%cd%,target=/code %DOCKER_IMAGE% xmlatex %str%

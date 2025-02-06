2e3116cd15b747b3f321bba97a36c9d2267f54fb
Bart Snapp <bart.snapp@gmail.com>
2025-02-06 15:28:52 -0500

    added these files for deploy

--- xmScripts/xmlatex.bat
+++ xmScripts/xmlatex.bat
@@ -1,7 +1,8 @@
 @echo off
 set str=%*
 set "str=%str:\=/%"
-set "DOCKER_IMAGE=ghcr.io/ximeraproject/xake2019:v2.1.1"
+set "DOCKER_IMAGE=ghcr.io/ximeraproject/ximeralatex:v2.5.8"
+@REM set "DOCKER_IMAGE=ghcr.io/ximeraproject/xake2024:v2.4.2"
 echo Starting docker %DOCKER_IMAGE% with args  %str%  in directory %cd%.
 echo docker run --rm --net="host" --mount type=bind,source=%cd%,target=/code %DOCKER_IMAGE% xmlatex %str%
 docker run --rm --net="host" --mount type=bind,source=%cd%,target=/code %DOCKER_IMAGE% xmlatex %str%

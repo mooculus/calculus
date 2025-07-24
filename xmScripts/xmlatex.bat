bc6da2e405e9c56f496debd49a9575944817350f
Wim Obbels <wim@obbels.be>
2025-07-24 17:08:27 +0200

    update to v2.7.2 (but use latest)

--- xmScripts/xmlatex.bat
+++ xmScripts/xmlatex.bat
@@ -1,7 +1,7 @@
 @echo off
 set str=%*
 set "str=%str:\=/%"
-set "DOCKER_IMAGE=ghcr.io/ximeraproject/ximeralatex:v2.6.8"
+set "DOCKER_IMAGE=ghcr.io/ximeraproject/ximeralatex:v2.7.2"
 echo Starting docker %DOCKER_IMAGE% with args  %str%  in directory %cd%.
 echo docker run --rm --net="host" --mount type=bind,source=%cd%,target=/code %DOCKER_IMAGE% xmlatex %str%
 docker run --rm --net="host" --mount type=bind,source=%cd%,target=/code %DOCKER_IMAGE% xmlatex %str%
